4b93fb42225d22645985e67d2ddd903b1aab81a9
Sebastian Wålinder <s.walinder@gmail.com>
2020-05-11 09:22:43 +0200

    Fixed bug where window did not maximize

--- src/Main.hs
+++ src/Main.hs
@@ -108,8 +108,8 @@ main = do
   myMVar <- newEmptyMVar
   runSDL
     True
-    S.Windowed
-    "FRP Lunar Lander"
+    S.Maximized
+    "Simple game"
     getResources
     ( \savedGameState renderer senseInput resources -> do
         _ <- reactimate (pure NoEvent) senseInput (\_ -> render renderer resources) (loadOldGameState update savedGameState myMVar)
